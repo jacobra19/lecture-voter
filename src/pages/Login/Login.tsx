206a456a34e8ea7e49508d9b706b2ceb4167f485
Yakov Rakhamimov <jacobra19@gmail.com>
2021-09-03 05:04:16 +0300

    protected routes infa start

---
+++ src/pages/Login/Login.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { HStack, PinInput, PinInputField } from '@chakra-ui/react';
+const Login = () => {
+	return (
+		<div
+			style={{
+				width: '100%',
+				height: 'calc( 100vh - 40px )',
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+			}}
+		>
+			<HStack>
+				<PinInput
+					autoFocus
+					otp
+					onChange={(value) => {
+						console.log(`onChange value`, value);
+					}}
+					onComplete={(value) => {
+						console.log(`onComplete value`, value);
+						// TODO: handle redirect to home page
+					}}
+				>
+					<PinInputField />
+					<PinInputField />
+					<PinInputField />
+					<PinInputField />
+				</PinInput>
+			</HStack>
+		</div>
+	);
+};
+
+export default Login;
