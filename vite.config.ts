5635db7a78cbc74fcb172b3c842e9599c831ebc0
Yakov Rakhamimov <jacobra19@gmail.com>
2021-09-04 08:23:20 +0300

    fixed aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite';
 import reactRefresh from '@vitejs/plugin-react-refresh';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [reactRefresh()],
+    alias: {
+        '@pages': path.resolve(__dirname, './src/pages'),
+        '@components': path.resolve(__dirname, './src/components'),
+        '@hooks': path.resolve(__dirname, './src/hooks'),
+    },
 });
