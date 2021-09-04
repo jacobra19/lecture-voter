d7861ba216fa6d2dd846e6f24ff4f720d2959635
Yakov Rakhamimov <jacobra19@gmail.com>
2021-09-05 00:08:50 +0300

    github login succesful

---
+++ src/lib/firebase/index.ts
@@ -0,0 +1,50 @@
+import { initializeApp } from 'firebase/app';
+import {
+    GithubAuthProvider,
+    signOut,
+    getAuth,
+    signInWithPopup,
+} from 'firebase/auth';
+
+// TODO: Replace the following with your app's Firebase project configuration
+const firebaseConfig = {
+    apiKey: 'AIzaSyDi2XLCoSAlKJWzpwHmPqwR47ZE50cvr0g',
+    authDomain: 'lecture-voter.firebaseapp.com',
+    projectId: 'lecture-voter',
+    storageBucket: 'lecture-voter.appspot.com',
+    messagingSenderId: '169844344401',
+    appId: '1:169844344401:web:daa72a9eace748572fda70',
+    measurementId: 'G-2QP7DRBL3C',
+};
+
+const app = initializeApp(firebaseConfig);
+
+const gitHubProvider = new GithubAuthProvider();
+
+const auth = getAuth();
+
+const signInWithGithub = async () => {
+    signInWithPopup(auth, gitHubProvider)
+        .then((result) => {
+            // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+            const credential = GithubAuthProvider.credentialFromResult(result);
+            const token = credential?.accessToken;
+
+            // The signed-in user info.
+            const user = result.user;
+            console.log(`user`, user);
+            // ...
+        })
+        .catch((error) => {
+            // Handle Errors here.
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            // The email of the user's account used.
+            const email = error.email;
+            // The AuthCredential type that was used.
+            const credential = GithubAuthProvider.credentialFromError(error);
+            // ...
+        });
+};
+
+export { app, gitHubProvider, auth, signInWithGithub };
--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,6 +1,7 @@
 import { Brand } from '@components';
 import React from 'react';
-
+import { Button } from '@chakra-ui/react';
+import { signInWithGithub } from '../../lib/firebase';
 const Login = () => {
     return (
         <div
@@ -10,9 +11,24 @@ const Login = () => {
                 display: 'flex',
                 alignItems: 'center',
                 justifyContent: 'center',
+                flexDirection: 'column',
+                gap: 40,
             }}
         >
-            <Brand/>
+            <Brand />
+            <Button
+                onClick={() => {
+                    signInWithGithub()
+                        .then((res) => {
+                            console.log(`res`, res);
+                        })
+                        .catch((err) => {
+                            console.log(err);
+                        });
+                }}
+            >
+                Login
+            </Button>
         </div>
     );
 };
